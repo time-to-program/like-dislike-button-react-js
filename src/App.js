607cd506a21d92565bac7e6ef8d142aac2b78524
time-to-program <raviteja.venkatesh@costbo.com>
2022-09-29 16:07:50 +0530

    create like and dislike button with count

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [likeCount, setLikeCount] = useState(50);
+  const [dislikeCount, setDislikeCount] = useState(25);
+
+  const [activeBtn, setActiveBtn] = useState("none");
+
+  const handleLikeClick = () => {
+    if (activeBtn === "none") {
+      setLikeCount(likeCount + 1);
+      setActiveBtn("like");
+      return;
+    }
+
+    if (activeBtn === 'like'){
+      setLikeCount(likeCount - 1);
+      setActiveBtn("none");
+      return;
+    }
+
+    if (activeBtn === "dislike") {
+      setLikeCount(likeCount + 1);
+      setDislikeCount(dislikeCount - 1);
+      setActiveBtn("like");
+    }
+  };
+
+  const handleDisikeClick = () => {
+    if (activeBtn === "none") {
+      setDislikeCount(dislikeCount + 1);
+      setActiveBtn("dislike");
+      return;
+    }
+    
+    if (activeBtn === 'dislike'){
+      setDislikeCount(dislikeCount - 1);
+      setActiveBtn("none");
+      return;
+    }
+
+    if (activeBtn === "like") {
+      setDislikeCount(dislikeCount + 1);
+      setLikeCount(likeCount - 1);
+      setActiveBtn("dislike");
+    }
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="container">
+      <div className="btn-container">
+        <button
+          className={`btn ${activeBtn === 'like' ? 'like-active' : ''}`}
+          onClick={handleLikeClick}
         >
-          Learn React
-        </a>
-      </header>
+          <span className="material-symbols-rounded">thumb_up</span>
+          Like {likeCount}
+        </button>
+
+        <button
+          className={`btn ${activeBtn === 'dislike' ? 'dislike-active' : ''}`}
+          onClick={handleDisikeClick}
+        >
+          <span className="material-symbols-rounded">thumb_down</span>
+          Dislike {dislikeCount}
+        </button>
+      </div>
     </div>
   );
 }
